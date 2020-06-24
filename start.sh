cefdb6e2a01117f8271369c4cc966cebc1549b68
He Rui <53299146+CrazyHer@users.noreply.github.com>
2020-06-24 12:34:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ apt install clang vim curl wget git -y &&\
 echo "deb [trusted=yes arch=all] https://yadominjinta.github.io/files/ termux extras" >> $PREFIX/etc/apt/sources.list &&\
 pkg in atilo-cn -y &&\
 atilo pull ubuntu &&\
-echo '请设置code-server的访问密码，回车确认：'
+echo '请设置code-server的访问密码，回车确认：' &&\
 read code_password &&\
 cat \
 "echo '继续进行安装...' &&\
@@ -45,9 +45,11 @@ server {
 '>> /etc/nginx/sites-available/code-server &&\
 ln -s /etc/nginx/sites-available/code-server /etc/nginx/sites-enabled/ &&\
 cat 'code-server&;nginx'>>~/startcode.sh &&\
+chmod 777 ~/startcode.sh &&\
 echo '全部安装完成！以后进入ubuntu输入./startcode.sh就可以启动code-server服务器啦！' &&\
 rm -rf ~/continue.sh ||echo '继续安装失败！'
 ">> ~/.atilo/ubuntu/root/continue.sh &&\
+chmod 777 ~/.atilo/ubuntu/root/continue.sh &&\
 echo "已通过atilo成功安装Ubuntu子系统在termux内" &&\
 echo "请输入atilo run ubuntu进入ubuntu后" &&\
 echo "继续输入命令: ./continue.sh 继续进行安装code-server" ||\
